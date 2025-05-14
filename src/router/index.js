bbc2e4be08a2c41727f74342f19431d54edbe942
yibrino <yibrahmehari128@gmail.com>
2025-05-14 02:07:59 +0200

    Data:json data added for testing

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,15 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import { components } from "vuetify/dist/vuetify.js";
 import Dashboard from "@/pages/index.vue";
+// import EmployeesPage from "@/pages/EmployeesPage.vue";
 const routes = [
   {
     path: "/",
     name: "Dashboard",
     component: Dashboard,
+    // children: [
+    //   { path: "/employees", name: "employees", component: EmployeesPage },
+    // ],
   },
 ];
 const router = createRouter({
